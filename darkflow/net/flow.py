1d474ac7358b564a6bb6b65b788befd36672962f
Patrícia Mika Sakugawa <32035844+pmikas@users.noreply.github.com>
2020-05-26 14:30:10 -0300

    Update flow.py

--- darkflow/net/flow.py
+++ darkflow/net/flow.py
@@ -17,7 +17,7 @@ pool = ThreadPool()
 def _save_ckpt(self, step, loss_profile):
     file = '{}-{}{}'
     model = self.meta['name']
-    backup = '/content/gdrive/My Drive/Cellere/Yolo/display/ckpt/'
+    backup = '/content/gdrive/My Drive/Facens/frui_detection/ckpt/'
     profile = file.format(model, step, '.profile')
     profile = os.path.join(backup, profile)
     with open(profile, 'wb') as profile_ckpt: 
